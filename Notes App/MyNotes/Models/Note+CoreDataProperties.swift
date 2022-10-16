115b2458fc5d6bd51b39dea38ff7944b427481f6
Festim Braha <52571480+festimbraha@users.noreply.github.com>
2022-10-16 21:37:16 +0200

    Kodi i kompletuar i Projektit

---
+++ Notes App/MyNotes/Models/Note+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CoreData
+
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var id: UUID!
+    @NSManaged public var text: String!
+    @NSManaged public var lastUpdated: Date!
+
+}
+
+extension Note : Identifiable {
+
+}
---
+++ Notes App/MyNotes/Views/EditNote/EditNoteViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class EditNoteViewController: UIViewController {
+    
+    static let identifier = "EditNoteViewController"
+    
+    var note: Note!
+
+    @IBOutlet weak private var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.text = note?.text
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        textView.becomeFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK:- Methods to implement
+    private func updateNote() {
+        note.lastUpdated = Date()
+        CoreDataManager.shared.save()
+    }
+    
+    private func deleteNote() {
+        CoreDataManager.shared.deleteNote(note)
+    }
+}
+
+// MARK:- UITextView Delegate
+extension EditNoteViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        note?.text = textView.text
+        if note?.title.isEmpty ?? true {
+            deleteNote()
+        } else {
+            updateNote()
+        }
+    }
+}
